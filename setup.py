1bdd2de6dbc0f66e9a68b5e1b8111b010933f17f
siasi <siasi@github.com>
2012-12-19 16:29:50 +0100

    Classifiers not properly managed by distutils

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 
-cc = """Programming Language :: Python
-License :: OSI Approved :: BSD License
-Operating System :: OS Independent
-Development Status :: 3 - Alpha
-Environment :: Console
-Intended Audience :: Developers
-Topic :: Software Development :: User Interfaces
-Topic :: Software Development :: Libraries :: Python Modules
-Topic :: System :: Logging
-Topic :: System :: Shells
-"""
+#cc = """License :: OSI Approved :: BSD License
+#Operating System :: OS Independent
+#Development Status :: 3 - Alpha
+#Environment :: Console
+#Intended Audience :: Developers
+#Topic :: Software Development :: User Interfaces
+#Topic :: Software Development :: Libraries :: Python Modules
+#Topic :: System :: Logging
+#Topic :: System :: Shells
+#"""
 
 setup(
 	name = 'batchcli',
@@ -19,6 +18,6 @@ setup(
 	description = 'A simple API to run tasks in batch mode and track progress on CLI.',
 	url = 'https://github.com/siasi/batchcli',
 	author = 'Stefano Iasi',
-	author_email = 'siasi@cisco.com',
-	classifiers = cc
+	author_email = 'siasi@cisco.com'
+#	classifiers = cc
 	)
